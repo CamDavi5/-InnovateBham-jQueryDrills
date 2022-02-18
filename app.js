8a3ae65128fbe6ddc1a309ca5cfb4649b946b7d5
CamDavi5 <camrdavis.biz@gmail.com>
2022-02-18 10:24:25 -0600

    jQuery variables updated and disable included

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
-const submitBtn = $('#btnSubmit');
+const $submitBtn = $('#btnSubmit');
+$("#btnSubmit").attr("disabled", true);
 //console.log(input);
 
 /*$("body").append('<div></div>');
 const newDiv = $('div')*/
 $('body').append('<ul></ul>');
-const newUl = $('ul');
+const $newUl = $('ul');
 
-$(submitBtn).click(function(e) {
-    let input = $('input[type=text]').val();
-    console.log(input);
-    if (input != "") {
-        /*let h2 = $('<h2></h2>');
-        h2.append(input);
-        newDiv.append(h2);
-        $(h2).mouseover(function() {
-            $(h2).css({
-                'background-color': 'green',
-                'border-radius': '30px'
-            })
-        })*/
+$($submitBtn).click(function (e) {
+    let $input = $('input[type=text]').val();
+    console.log($input);
 
-        let newList = $('<li></li>');
-        newList.append(input);
-        $('body').append(newList)
-        $(newList).click(function() {
-            let randomColor = Math.floor(Math.random()*16777215).toString(16);
-            $(newList).css('color', '#'+randomColor);
+    /*let h2 = $('<h2></h2>');
+    h2.append(input);
+    newDiv.append(h2);
+    $(h2).mouseover(function() {
+        $(h2).css({
+            'background-color': 'green',
+            'border-radius': '30px'
         })
-        $(newList).dblclick(function() {
-            newList.remove();
-        })
-        //alert(input);
-    } 
+    })*/
+
+    let $newList = $('<li></li>');
+    $newList.append($input);
+    $($newUl).append($newList)
+    $($newList).click(function () {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        $($newList).css('color', '#' + randomColor);
+    })
+    $($newList).dblclick(function () {
+        $newList.remove();
+    })
+    //alert(input);
+
     e.preventDefault();
 })
+
+$("input[type='text']").on("keyup", function () {
+    const $inputVal = $("input[type='text']").val();
+
+    if ($inputVal == "") {
+        $("#btnSubmit").attr("disabled", true);
+    } else {
+        $("#btnSubmit").attr("disabled", false);
+    }
+});
 
 
 
